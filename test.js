e57b5470be18e07f2a8bd72945c23d915c3b2d73
Kumar <kumarma@hyd-kumarma-m.ds.ad.adp.com>
2018-02-01 08:43:54 +0530

    remove dummy test file

--- test.js
+++
@@ -1,8 +0,0 @@
-const db = require("./db.js");
-let users = db.useCollection("users"); // will create and return collection called "users"
-users.insert({"name":"Logan"});
-users.insert({"name":"Batman"});
-
-console.log(users.find({})) //will return all the records for users collection
-
-console.log(users.find({"name":"Batman"})); //will return the matched record
